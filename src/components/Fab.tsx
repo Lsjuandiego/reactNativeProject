1e330144beffb6e83327c15439ce097dbeb60315
lsjuandiego <jlopez@almeraim.com>
2024-01-17 15:57:25 -0500

    codigo especifico para diferentes os

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native';
+import { View, Text, StyleSheet, TouchableNativeFeedback, Platform, TouchableOpacity } from 'react-native';
 
 interface Props {
     title: string;
@@ -8,27 +8,46 @@ interface Props {
 }
 
 export const Fab = ({ title, position = 'br', onPress }: Props) => {
-    return (
-        <View style={[styles.fabLocation,
-        (position === 'bl') ? styles.left : styles.right
-        ]}>
-            <TouchableNativeFeedback
 
+    const ios = () => {
+        return (
+            <TouchableOpacity
                 onPress={onPress}
-                background={ TouchableNativeFeedback.Ripple('#28425B', false, 30)}
-                >
-                
+                activeOpacity={0.75}
+                style={[styles.fabLocation,
+                (position === 'bl') ? styles.left : styles.right
+                ]}
+            >
                 <View style={styles.fab}>
-                    <Text style={styles.fabText}>
-                        {title}
-                    </Text>
+                    <Text style={styles.fabText}> {title} </Text>
                 </View>
-            </TouchableNativeFeedback>
-        </View>
+            </TouchableOpacity>
+        )
+    }
 
-    )
+    const android = () => {
+        return (
+            <View style={[styles.fabLocation,
+            (position === 'bl') ? styles.left : styles.right
+            ]}>
+                <TouchableNativeFeedback
+
+                    onPress={onPress}
+                    background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}
+                >
+
+                    <View style={styles.fab}>
+                        <Text style={styles.fabText}>
+                            {title}
+                        </Text>
+                    </View>
+                </TouchableNativeFeedback>
+            </View>
+        )
+    }
+
+    return (Platform.OS === 'ios') ? ios() : android();
 }
-
 const styles = StyleSheet.create({
     fabLocation: {
         position: 'absolute',
