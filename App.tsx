a6a37133bd5db50e1a5b0c3388c59324abe2705d
lsjuandiego <jlopez@almeraim.com>
2024-01-17 11:29:50 -0500

    creacion pantallas independientes

--- App.tsx
+++ App.tsx
@@ -1,22 +1,8 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 export const App = () => {
   return (
-    <View style={
-      {
-        flex: 1,
-        justifyContent: 'center'
-      }
-    }>
-      <Text style ={
-        {
-          fontSize: 45,
-          textAlign: 'center',
-        }
-      } >Hola mundo</Text>
-    </View>
+    <HolaMundoScreen/>
   )
 }
-
-export default App;
