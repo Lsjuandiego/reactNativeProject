09d1a1f9c121f91db55e453fba5fc2008d92a4aa
lsjuandiego <jlopez@almeraim.com>
2024-01-18 10:10:34 -0500

    #96 realizacion calculos

--- src/screens/calculadoraScreen.tsx
+++ src/screens/calculadoraScreen.tsx
@@ -103,6 +103,27 @@ export const CalculadoraScreen = () => {
         ultimaOperacion.current = Operadores.sumar;
     }
 
+    const calcular = () => {
+        const num1 = Number(numero);
+        const num2 = Number(numeroAnterior);
+
+        switch (ultimaOperacion.current) {
+            case Operadores.sumar:
+                setNumero(`${num1 + num2}`);
+                break;
+            case Operadores.restar:
+                setNumero(`${num2 - num1}`);
+                break;
+            case Operadores.dividir:
+                setNumero(`${num2 / num1}`);
+                break;
+            case Operadores.multiplicar:
+                setNumero(`${num1 * num2}`);
+                break;
+        }
+        setNumeroAnterior('0');
+    }
+
     return (
         <View style={styles.calculadoraContainer}>
             {
@@ -155,7 +176,7 @@ export const CalculadoraScreen = () => {
                 {/* boton */}
                 <BotonCalculadora texto='0' ancho accion={armarNumero} />
                 <BotonCalculadora texto='.' accion={armarNumero} />
-                <BotonCalculadora texto='=' color='#FF9427' accion={limpiar} />
+                <BotonCalculadora texto='=' color='#FF9427' accion={calcular} />
             </View>
         </View>
     )
